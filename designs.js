4c55c787ab39750ec70eb5fbae2b18d0bf1e6fe1
Ted Liu <mr.liu0518@outlook.com>
2021-08-05 18:08:01 +0800

    1

--- designs.js
+++ designs.js
@@ -4,7 +4,10 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
+    const height = document.getElementById("inputHeight");
+    const width = document.getElementById("inputWidth");
+    const submit = document.getElementById("submit");
 // Your code goes here!
-
 }
+
+submit.addEvent
