a084d4903f94c626d8bdb400891f6b02f75cecff
Ted Liu <mr.liu0518@outlook.com>
2021-08-09 19:48:02 +0800

    1

--- designs.js
+++ designs.js
@@ -1,30 +1,29 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-
-
+// When submit button is clicked by the user, call makeGrid()
 const button = document.getElementById("button");
 button.addEventListener("click",function(){
-    makeTable()});
+    makeGrid()});
 
-function makeTable() 
+function makeGrid() 
 {   
-    console.log("makeTable")
+    console.log("makeGrid")
+    // Select size input
     const height = document.getElementById("inputHeight").value;
     const width = document.getElementById("inputWidth").value;
+    // Select table
     const table = document.getElementById("pixelCanvas");
-
+    // Remove previous grid 
     while(table.firstChild){
         table.removeChild(table.firstChild)
     }
+    // Create rows and append to table
     for (let i = 1; i <= height; i++) {
         const tr = document.createElement('tr');
         table.appendChild(tr);
+        // Create Columns and append to rows
         for (let i = 1; i <= width; i++) {
             const td = document.createElement('td');
             tr.appendChild(td);
+            // Add Click Event
             td.addEventListener('click', function(){
                 console.log('A block was clicked.');
                 changeColor(td);
@@ -34,6 +33,7 @@ function makeTable()
 }
 
 function changeColor(element){
+    // Select color input
     const color = document.getElementById("colorPicker").value;
     console.log("Change color");
     element.style.backgroundColor = color;
